4ac0aaa5a8a287f51545193627c4370b76408e46
akshay-5557 <akshaymurugesan93@gmail.com>
2026-02-23 18:10:10 +0530

    login page made

---
+++ the-grouse-gym-frontend/src/api/auth.api.ts
@@ -0,0 +1,7 @@
+import api from "./axios"
+
+export const loginUser = async (username: string, password: string) => {
+    const res = await api.post("/auth/login", {username, password});
+    return res.data;
+};
+
