56b1b6b2f65f5617e61c9567659f300744fd62a2
akshay-515 <akshay.murugesan93@gmail.com>
2026-03-28 13:44:42 +0530

    to check

--- gym-backend/src/routes/memberships.routes.ts
+++ gym-backend/src/routes/memberships.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getMemberships } from "../controllers/memberships.controller";
+import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
+router.use(authenticate);
 router.get("/" , getMemberships);
 
 export default router;
